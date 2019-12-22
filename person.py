02e6b0162f36272e07e446422e162c120a553f0a
atleastzero <megan.obryan52@gmail.com>
2019-12-21 22:27:22 -0800

    Added parse_dump to grab info when person is create. Not working yet.

--- person.py
+++ person.py
@@ -1,9 +1,20 @@
+import re
+
 class Person:
     def __init__(self, person_id, file_dump):
         self.person_id = person_id
-        self.name = "NOT A NAME"
+        self.name = ""
         self.data = file_dump
         self.families = []
+        self.parse_dump()
+
+    def parse_dump(self):
+        for line in self.data:
+            if re.search("NAME", line):
+                try:
+                    self.name = re.search(r"NAME [\w\s/'-]+", line).group()[5:]
+                except:
+                    self.name = "Unknown"
 
     def __str__(self):
         return "Person with id " + str(self.person_id) + " is named " + str(self.name)
