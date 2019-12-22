679368baf08c08002174853c6f0f988d379cf02d
atleastzero <megan.obryan52@gmail.com>
2019-12-21 22:27:51 -0800

    Prevent index out of range issue in tree Builder

--- tree_builder.py
+++ tree_builder.py
@@ -10,7 +10,7 @@ class Builder:
         if f.mode == 'r':
             self.lines = f.read().splitlines()
         f.close()
-        self.people = []
+        self.people = {}
         self.families = {}
         self.loop_for_people()
 
@@ -21,20 +21,32 @@ class Builder:
     def loop_for_people(self):
         try:
             current_index = 0
-            print(self.lines[current_index])
-            while self.lines[current_index]:
+            while current_index < len(self.lines):
                 print("looking for person")
                 if re.search("@P", self.lines[current_index]):
                     print("found a person")
+                    person_id = re.search(r"@P[\d]+", self.lines[current_index]).group()[2:]
+                    print(person_id)
                     current_index += 1
+                    if current_index >= len(self.lines):
+                        break
                     data = []
                     while not re.search("@P", self.lines[current_index]):
                         data.append(self.lines[current_index])
+                        current_index += 1
+                        if current_index >= len(self.lines):
+                            break
+                    current_index -= 1
+                    if current_index >= len(self.lines):
+                        break
+                    new_person = person.Person(person_id, data)
+                    self.people[person_id] = new_person
+                current_index += 1
         except Exception as e:
             print(e)
 
     def print_people(self):
-        for person in self.people:
+        for person in self.people.values():
             print(person)
 
 def tests():
